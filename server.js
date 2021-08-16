1fdebf542fc36d28e07b6fb561eb0992f6e377e5
ugonchiamaka-dev <chiamakajudith1@gmail.com>
2021-08-16 17:42:58 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const port = process.env.PORT || 5000;
+const mongoose = require("mongoose");
+
+const app = express();
+mongoose.connect(
+  "mongodb+srv://gZOFHhVBWYypd25Z:gZOFHhVBWYypd25Z@cluster0.uzqil.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
+
+mongoose.connection
+  .once("open", () => {
+    console.log("Database connected sucessfully");
+  })
+  .on("error", () => {
+    console.log("Error while connecting to the database");
+  });
+
+app.listen(port, () => {
+  console.log(`server sucessfully created on port ${port}`);
+});
